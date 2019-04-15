b621083c10bcdeefd2221981ebd95cc4d2c27297
gestap <gestap@gmail.com>
2019-04-15 15:09:41 +0300

    Solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,15 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    this.reverse();
+    let a = this[this.length - 1];
+    if (this.length === 0) {
+      return undefined;
+    } else {
+      this.length--;
+    }
+    this.reverse();
+    return a;
   };
 }
 
