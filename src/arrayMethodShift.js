8346b57567c06f30c0b4ce6e9bb746ad93b95874
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-06-19 16:21:54 +0300

    shift

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,17 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    if (!this.length) {
+      return undefined;
+    }
+
+    const element = this[0];
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+
+    this.length -= 1;
+    return element;
   };
 }
 
