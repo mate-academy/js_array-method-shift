61f004c2ce9e4f73abec6104a92a7dbf02997201
RavenVol <r.volianiuk@gmail.com>
2019-06-20 13:10:28 +0300

    Supervisor's comments accepted and applied

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,20 +6,15 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     // write code here
-    const reservArray = [];
     const item2BLost = this[0];
 
-    for (let i = 0; i < this.length; i++) {
-      reservArray[i] = this[i];
+    for (let i = 1; i < this.length; i++) {
+      this[i - 1] = this[i];
     }
 
     this.length > 0
       ? this.length = this.length - 1
       : null;
-
-    for (let i = 1; i < reservArray.length; i++) {
-      this[i - 1] = reservArray[i];
-    }
 
     return this.length > 0
       ? item2BLost
