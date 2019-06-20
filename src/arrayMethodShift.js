e1a0f69a50bbabeddce0d72ff5d1259647335037
neitiris <veliantos@gmail.com>
2019-06-20 16:31:36 +0300

    Solution commit

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,6 +6,16 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     // write code here
+    if (this.length > 0) {
+      const deleted = this[0];
+      for (let i = 0; i < this.length; i++) {
+        this[i] = this[i + 1];
+      }
+      this.length = this.length - 1;
+      return deleted;
+    } else {
+      return undefined;
+    }
   };
 }
 
