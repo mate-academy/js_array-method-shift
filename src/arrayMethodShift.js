a22b92598064a82be81d36da6e28b87fad608cf0
artem-meleshko <timonlondon@hotmail.com>
2019-06-21 14:03:29 +0300

    Solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,16 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    const deletedItem = this[0];
+
+    if (this.length > 0) {
+      for (let i = 0; i < this.length - 1; i++) {
+        this[i] = this[i + 1];
+      }
+      this.length--;
+    }
+
+    return deletedItem;
   };
 }
 
