efffb1906ce80c0cebfdd881dc03ad0732d89375
Taisiia <taisiia.pomazan@gmail.com>
2019-06-22 13:18:22 +0300

    sol

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,7 +6,17 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     // write code here
+    const firstElem = this[0];
+
+    if (this.length > 0) {
+      for (let i = 0; i < this.length; i++) {
+        this[i] = this[i + 1];
+      };
+
+      this.length--;
+    };
+    return firstElem;
   };
-}
+};
 
 module.exports = applyCustomShift;
