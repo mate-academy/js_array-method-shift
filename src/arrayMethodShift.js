06b831f1c1ba4ab031ea7e62b8e302b97e080576
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-08-16 16:45:50 +0300

    fix return

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -8,12 +8,14 @@ function applyCustomShift() {
     if (this.length === 0) {
       return undefined;
     }
+
     const firstOfThis = this[0];
 
     for (let i = 1; i < this.length; i++) {
       this[i - 1] = this[i];
     }
     this.length--;
+
     return firstOfThis;
   };
 }
