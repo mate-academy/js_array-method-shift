045b5e633ff58dccedab79a7b20fe0936f826867
youvovk <wer3wollf@gmail.com>
2019-08-18 13:53:59 +0300

    solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,28 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    const saveArr = [];
+
+    let firstElem;
+
+    let count = 0;
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === 0) {
+        firstElem = this[i];
+      } else {
+        saveArr[count] = this[i];
+        count++;
+      }
+    }
+
+    this.length = 0;
+
+    for (let i = 0; i < saveArr.length; i++) {
+      this[i] = saveArr[i];
+    }
+
+    return firstElem;
   };
 }
 
