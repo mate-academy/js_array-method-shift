971e90470b670d8efe0a1d9c039f0605949a7cc4
Aleks <alex.andrutskii@gmail.com>
2019-08-19 23:16:20 +0300

    corrections according to comments

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,16 +5,16 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    const firstElement = this[0];
-
     if (this.length === 0) {
-      return;
+      return undefined;
     }
+
+    const firstElement = this[0];
 
     for (let i = 0; i < this.length - 1; i++) {
       this[i] = this[i + 1];
     }
-    this.length = this.length - 1;
+    this.length -= 1;
 
     return firstElement;
   };
