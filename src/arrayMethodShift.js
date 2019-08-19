38b3c01f58a8ce151a7f619f9e069ff5870ff854
KosBelozyorov <KosBelozyorov@gmail.com>
2019-08-19 15:50:10 +0300

    fix

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,11 +5,11 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    const firstElement = this[0];
-
     if (this.length === 0) {
       return undefined;
     }
+
+    const firstElement = this[0];
 
     for (let i = 0; i < this.length; i += 1) {
       this[i] = this[i + 1];
