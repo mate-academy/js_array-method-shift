17162d11e03e61e5ffd0becfc218b8695769ff90
KseniiaMazan <ksusamazan@gmail.com>
2019-08-20 23:39:13 +0300

    solution without arr methods

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -7,12 +7,13 @@ function applyCustomShift() {
   [].__proto__.shift2 = function() {
     const returnItem = this[0];
 
-    if (this.length) {
-      this.reverse();
-      this.length = this.length - 1;
-      this.reverse();
+    for (let i = 1; i < this.length; i++) {
+      this[i - 1] = this[i];
     }
 
+    if (this.length) {
+      this.length--;
+    }
     return returnItem;
   };
 }
