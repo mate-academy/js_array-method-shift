a0203bada3af40a349df212bd2495d9df7fdb0cb
VladyslavSak <iblackvour@gmail.com>
2019-08-20 11:30:04 +0300

    Fix, without methods to array

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,16 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    return this.splice(0, 1)[0];
+    const shiftedList = [...this];
+    const shiftedElement = this[0];
+
+    this.length = 0;
+
+    for (let i = 0; i < shiftedList.length - 1; i++) {
+      this[i] = shiftedList[i + 1];
+    }
+
+    return shiftedElement;
   };
 }
 
