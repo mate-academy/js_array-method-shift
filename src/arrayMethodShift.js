c49345415f0de8f9b261340ad125a5f1198cc288
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-08-21 23:40:12 +0300

    'Solution'

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,12 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    const returned = this[0];
+    for (let i = 0; i < this.length - 1; i++) {
+      this[i] = this[i + 1];
+    }
+    this.splice(this[this.length - 1], 1);
+    return returned;
   };
 }
 
