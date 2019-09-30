5cec19f3a86e3f6d88119986edafa9d01bbbac34
VadimK <djvadya16@gmail.com>
2019-09-30 21:34:43 +0300

    done

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    if (!this.length) {
+      return undefined;
+    } else {
+      const propShift = this[0];
+      let placeNum = 1;
+
+      for (const value in this) {
+        this[value] = this[placeNum];
+        placeNum++;
+      }
+      this.length--;
+
+      return propShift;
+    }
   };
 }
 
