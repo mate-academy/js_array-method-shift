086847730cb2ebe0463d13081726fe4a949e5894
Oles Marola <oles_pw@ukr.net>
2019-11-19 17:18:14 +0200

    shift

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,17 +5,18 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
+    if (this.length < 1) {
+      return undefined;
+    }
+
     const firstElement = this[0];
     const afterShift = [];
+
     for (let i = 1; i < this.length; i++) {
       afterShift[i - 1] = this[i];
       this[i - 1] = afterShift[i - 1];
     }
-    if (this.length < 1) {
-      return undefined;
-    } else {
-      this.length = this.length - 1;
-    }
+    this.length = this.length - 1;
     return firstElement;
   };
 }
