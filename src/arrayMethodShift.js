d808bb0eef7b2234b5748e037dd52311fc3293c5
bandura <banduraweb@ukr.net>
2019-11-20 03:11:06 +0200

    Solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,13 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    const firstItem = this[0];
+    const selfCopy = [...this];
+    this.length = 0;
+    for (let i = 0; i < selfCopy.length - 1; i++) {
+      this[i] = selfCopy[i + 1];
+    }
+    return firstItem;
   };
 }
 
