16340fd5a159ee1af7a162ecb94e760ad2ab900e
Anna <anna.lyrynenko@gmail.com>
2020-01-24 19:01:37 +0200

    solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,19 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    if (!this.length) {
+      return undefined;
+    }
+
+    const deleted = this[0];
+
+    for (let i = 0; i < this.length - 1; i++) {
+      this[i] = this[i + 1];
+    }
+
+    this.length = this.length - 1;
+
+    return deleted;
   };
 }
 
