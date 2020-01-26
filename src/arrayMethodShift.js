53b9fbc2622bf76fa1f68cfd53c5f1e65da8d281
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-26 12:05:50 +0200

    fix solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -7,7 +7,15 @@ function applyCustomShift() {
   [].__proto__.shift2 = function() {
     const deleted = this[0];
 
-    this.splice(0, 1);
+    if (!this.length) {
+      return undefined;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+
+    this.length--;
 
     return deleted;
   };
