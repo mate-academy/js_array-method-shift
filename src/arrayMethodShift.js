bca69c0384df3c3c6f04125f1204b22f71bc910f
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-01-27 18:09:28 +0200

    fixed

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,11 +5,11 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    const firstItem = this[0];
-
     if (!this.length) {
-      return undefined;
+      return;
     }
+
+    const firstItem = this[0];
 
     for (let i = 0; i < this.length; i++) {
       this[i] = this[i + 1];
