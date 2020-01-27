37f55c42d0857a3989eb1f2121c6323ed7024acf
Senya Volkov <formobileprima@gmail.com>
2020-01-27 15:21:29 +0200

    fixed remarks

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,17 +5,16 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    if (this.length === 0) {
+    if (!this.length) {
       return undefined;
     }
 
-    const mainArr = Array.from(this);
-    const result = mainArr[0];
+    const result = Array.from(this)[0];
 
-    for (let i = 0; i < mainArr.length; i++) {
+    for (let i = 0; i < this.length; i++) {
       this[i] = this[i + 1];
     }
-    this.length = this.length - 1;
+    this.length--;
 
     return result;
   };
