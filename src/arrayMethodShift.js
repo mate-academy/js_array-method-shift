d2c3b06a33c4c04727e0d0296664d622345be1b7
Olexander <kravchenko.sania@gmail.com>
2020-01-27 22:52:48 +0200

    second attempt

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,14 +5,14 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    const firstValue = this[0];
-
-    if (this.length < 1) {
+    if (!this.length) {
       return undefined;
     }
 
-    for (let i = 1; i < this.length; ++i) {
-      this[i - 1] = this[i];
+    const firstValue = this[0];
+
+    for (let i = 0; i < this.length - 1; i++) {
+      this[i] = this[i + 1];
     }
 
     this.length--;
