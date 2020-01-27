016d4657b8591f65ec944ecdf8db77abe9e63db1
Actireon <eheieiei37@gmail.com>
2020-01-27 16:45:39 +0200

    refactoring code

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -7,12 +7,12 @@ function applyCustomShift() {
   [].__proto__.shift2 = function() {
     const firstElement = this[0];
 
-    if (this.length === 0) {
+    if (!this.length) {
       return undefined;
-    } else {
-      for (let i = 1; i < this.length; i++) {
-        this[i - 1] = this[i];
-      }
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      this[i - 1] = this[i];
     }
 
     this.length--;
