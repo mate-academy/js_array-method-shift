4aa21a7975580d9a5be45bbb20d16606cd94dd67
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-01-27 18:42:16 +0200

    solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    if (!this.length) {
+      return undefined;
+    }
+
+    const firstElement = this[0];
+
+    delete this[0];
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+    this.length -= 1;
+
+    return firstElement;
   };
 }
 
