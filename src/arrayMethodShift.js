b61c1650000267d453e05b3785b05fad0cb6a569
Natalia Lypovyk <cardinnata@gmail.com>
2020-01-27 14:46:42 +0200

    shift without splice

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,19 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    return this.splice(0, 1)[0];
+    const shiftedItem = this[0];
+
+    if (!this.length) {
+      return undefined;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+
+    this.length -= 1;
+
+    return shiftedItem;
   };
 }
 
