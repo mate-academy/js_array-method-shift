5e175374cc34f641302c707b801a806428f57df7
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-01-27 12:58:11 +0200

    so;ution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,6 +6,19 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const shiftValue = this[0];
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+
+    this.length -= 1;
+
+    return shiftValue;
   };
 }
 
