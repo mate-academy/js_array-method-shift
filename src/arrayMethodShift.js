d63498cbaea84b05400a8df354f9ae9bdf652473
Al145 <47367383+Al145@users.noreply.github.com>
2020-01-28 23:03:48 +0200

    solut

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,6 +6,19 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     // write code here
+    const removedElement = this[0];
+
+    if (this.length < 1) {
+      return undefined;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+
+    this.length--;
+
+    return removedElement;
   };
 }
 
