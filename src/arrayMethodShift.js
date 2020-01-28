eb8dbc457ee81b3d4afc46f47ad690c54b3da015
lutfie <serverieva.lutfie@gmail.com>
2020-01-28 11:38:04 +0200

    moved if before other actions

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,14 +5,14 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
+    if (!this.length) {
+      return undefined;
+    }
+
     const firstItem = this[0];
 
     for (let i = 0; i < this.length; i++) {
       this[i] = this[i + 1];
-    }
-
-    if (!this.length) {
-      return undefined;
     }
 
     this.length -= 1;
