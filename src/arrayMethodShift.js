8b7375662232cf374342656b2ff4fd44e24c5d0f
Maximka <maximkacmm@gmail.com>
2020-01-29 17:18:01 +0200

    solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,16 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    if (this.length) {
+      const pop = this[0];
+
+      for (let i = 0; i < this.length - 1; i++) {
+        this[i] = this[i + 1];
+      }
+      this.length -= 1;
+
+      return pop;
+    }
   };
 }
 
