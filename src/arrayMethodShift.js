dc09d6b1e790eb40606db16b052974ae241d8f8b
Maximka <maximkacmm@gmail.com>
2020-01-29 18:12:45 +0200

    fix commit

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,14 +6,15 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     if (this.length) {
-      const pop = this[0];
+      const shift = this[0];
 
       for (let i = 0; i < this.length - 1; i++) {
         this[i] = this[i + 1];
       }
+
       this.length -= 1;
 
-      return pop;
+      return shift;
     }
   };
 }
