530f75b74b50ef367168e2abe498e309378f7187
crash2bash <sachagurov@gmail.com>
2020-03-29 10:46:37 +0300

    solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,21 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    let shiftItem;
+
+    if (this[0] !== undefined) {
+      shiftItem = this[0];
+    } else {
+      return;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+
+    this.length = this.length - 1;
+
+    return shiftItem;
   };
 }
 
