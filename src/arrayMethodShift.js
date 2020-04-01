64e316e32fb42954973fc55f363a1963d720e96d
WincherZ <wincher2016@gmail.com>
2020-04-01 17:26:37 +0300

    some changes

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    const arr = this;
+    const item = arr[0];
+
+    if (arr.length < 1) {
+      return undefined;
+    };
+
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i + 1];
+    };
+
+    arr.length -= 1;
+
+    return item;
   };
 }
 
