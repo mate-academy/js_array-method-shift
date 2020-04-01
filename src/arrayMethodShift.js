9bb316e0a876390cb993c805a14702b08225a81d
romarios1987 <romios047@gmail.com>
2020-04-01 15:18:47 +0300

    updated IF

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -8,14 +8,12 @@ function applyCustomShift() {
     const items = this;
     const firstItem = items[0];
 
-    if (items.length <= 0) {
-      return undefined;
+    if (items.length > 0) {
+      for (let i = 0; i < items.length; i++) {
+        items[i] = items[i + 1];
+      }
+      items.length--;
     }
-
-    for (let i = 0; i < items.length; i++) {
-      items[i] = items[i + 1];
-    }
-    items.length--;
 
     return firstItem;
   };
