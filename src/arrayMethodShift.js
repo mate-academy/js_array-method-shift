0212950d0c46592c35af5e3a1f36f9b6d036be7c
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-01 18:03:23 +0300

    Solution v0.1

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,18 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    const items = this;
+    const item = items[0];
+
+    if (items.length < 1) {
+      return undefined;
+    }
+
+    for (let i = 0; i < items.length; i++) {
+      items[i] = items[i + 1];
+    };
+    items.length = items.length - 1;
+    return item;
   };
 }
 
