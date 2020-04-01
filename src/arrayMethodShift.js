3fe6f806569680ff9899093003f7706a029caf80
Andrey Faychak <faychak333@gmail.com>
2020-04-01 18:39:50 +0300

    'solution'

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,6 +6,20 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     // write code here
+    let popElement;
+
+    if (this.length === 0) {
+      return undefined;
+    } else {
+      popElement = this[0];
+
+      for (let i = 0; i < this.length - 1; i++) {
+        this[i] = this[i + 1];
+      }
+      this.length = this.length - 1;
+    }
+
+    return popElement;
   };
 }
 
