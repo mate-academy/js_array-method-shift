07ac33eec297aed2defd272759e5f69be6f581be
Yarik1993 <makarov1603@icloud.com>
2020-04-01 19:27:09 +0300

    'solution'

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,6 +6,18 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     // write code here
+    const answer = this[0];
+
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      this[0 + i - 1] = this[i];
+    }
+    this.length = this.length - 1;
+
+    return answer;
   };
 }
 
