e4aebe364726ca888cda69ee37bde4acc069904d
Vlad <oneviddi@gmail.com>
2020-04-01 22:09:32 +0300

    pre

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -4,8 +4,13 @@
  * Implement method shift
  */
 function applyCustomShift() {
-  [].__proto__.shift2 = function() {
-    // write code here
+  [].__proto__.shift2 = function(separator) {
+    /*
+    specify separator
+    let result = '';
+    check  whether it exist
+    finish it
+    */
   };
 }
 
