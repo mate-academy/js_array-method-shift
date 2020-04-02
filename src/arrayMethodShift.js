01bbea9c7155bd6f9660c647a4dcd9650ca09244
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-02 15:00:21 +0300

    solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -7,9 +7,15 @@ function applyCustomShift() {
   [].__proto__.shift2 = function() {
     const result = this[0];
 
-    if (this.length !== 0) {
-      this.splice(0, 1);
+    if (this.length === 0) {
+      return undefined;
     }
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+
+    this.length = this.length - 1;
 
     return result;
   };
