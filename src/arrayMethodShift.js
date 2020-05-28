b8c5547f238de7a994c953078a22fe39f63fda29
nazmeln <nazik100378@gmail.com>
2020-05-28 15:33:45 +0300

    solved

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -6,6 +6,16 @@
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
     // write code here
+    const removedElement = this[0];
+    const arr = [...this];
+
+    this.length = 0;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      this[i] = arr[i + 1];
+    }
+
+    return removedElement;
   };
 }
 
