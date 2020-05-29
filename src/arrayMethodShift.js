8b54e26a0b72b6f6e3e6ccc36823929efe7cad42
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-05-29 11:19:08 +0300

    fix

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,19 +5,19 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    const arr = [...this];
+    const returnedElement = [...this];
 
     if (!this.length) {
       return;
     }
 
-    for (let i = 0; i < arr.length - 1; i++) {
-      this[i] = arr[i + 1];
+    for (let i = 0; i < returnedElement.length - 1; i++) {
+      this[i] = returnedElement[i + 1];
     }
 
     this.length--;
 
-    return arr[0];
+    return returnedElement[0];
   };
 }
 
