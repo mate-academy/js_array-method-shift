d0fc901fea5b75a916e168ebae6514cadbbf5f55
Andrew Nickyforov <nik_andrey@ukr.net>
2020-05-30 16:58:23 +0300

    solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 'use strict';
 
 /**
@@ -5,7 +6,18 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    if (this.length === 0) {
+      return;
+    }
+
+    const deletedItem = this[0];
+
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+    this.length = this.length - 1;
+
+    return deletedItem;
   };
 }
 
