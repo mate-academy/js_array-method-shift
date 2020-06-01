667de4818ad960273b1f52384b2f52356a61d8fb
Rammsfan <Rammsfanhtw@gmail.com>
2020-06-01 14:51:54 +0300

    Solution

--- src/arrayMethodShift.js
+++ src/arrayMethodShift.js
@@ -5,7 +5,19 @@
  */
 function applyCustomShift() {
   [].__proto__.shift2 = function() {
-    // write code here
+    const firstElement = this[0];
+
+    if (this.length > 0) {
+      for (let i = 0; i < this.length; i++) {
+        this[i] = this[i + 1];
+      }
+    } else {
+      return undefined;
+    }
+
+    this.length -= 1;
+
+    return firstElement;
   };
 }
 
